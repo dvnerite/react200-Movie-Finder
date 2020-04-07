01db7e92f9c68222bf49f8e199b581d513bcbfeb
dvnerite <57559064+dvnerite@users.noreply.github.com>
2020-04-07 10:41:01 -0700

    step 4, having trouble with Webpack

---
+++ src/js/containers/MovieSearchContainer.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { render } from 'enzyme';
+
+class MovieSearchContainer extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+
+render() {
+    return (
+        <div>
+            <h1>Movie Search Container</h1>
+        </div>
+    )
+}
+
+}
+
+export default MovieSearchContainer;
